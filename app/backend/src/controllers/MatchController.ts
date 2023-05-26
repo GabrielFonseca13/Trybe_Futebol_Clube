6df75772d92eb47217d0ca846ff10976a6e7a58b
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-05-26 19:05:27 -0300

    refix Lint

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -44,9 +44,8 @@ async function createMatch(req: Request, res: Response) {
   const { homeTeamId, awayTeamId } = newMatchData;
 
   if (homeTeamId === awayTeamId) {
-    return res.status(422).json({ message: 'It is not possible to create a match with two equal teams' });
+    res.status(422).json({ message: 'It is not possible to create a match with two equal teams' });
   }
-
 
   const homeTeamsExists = await TeamService.findById(homeTeamId);
   if (!homeTeamsExists) return res.status(404).json({ message: 'There is no team with such id!' });
